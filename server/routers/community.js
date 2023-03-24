114acba8b94e3a9cc4141e69629005dd49f46b58
Parthtrap <mttparth306@gmail.com>
2023-03-24 22:00:35 +0530

    Routers Planned Out

---
+++ server/routers/community.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+
+// Get All Community
+
+// Create a Community
+
+// Delete a Community
+
+// Add tag
+
+// Delete tag
+
+// Edit Community Profile Pic
+
+// Edit Community Banner Pic
+
+// Make User Moderator
+
+// Remove User from Moderator
+
+// Pin a Post
+
+// Unpin a Post
+
+// Get all Reported Posts
+
+// Get community by name
+
+export default router;
