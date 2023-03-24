7c746cea3b0827b9d1014c556ded30162661daa2
Gauarv-Jain <jaing438@gmail.com>
2023-03-24 23:42:19 +0530

    navbar

---
+++ client/src/Pages/Components/Navbar.js
@@ -0,0 +1,161 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+
+  return (
+    <nav className="bg-fill shadow-base fixed top-0 z-50 w-full shadow-sm ">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex ">
+        <div className="">
+          <div className="flex items-center justify-between py-4 md:block">
+            <div className="md:hidden">
+              <button
+                className="text-primary focus:border-gray-400 p-2 rounded-md outline-none  focus:border"
+                onClick={() => {
+                  setNavbar(!navbar);
+                  setSidebar(false);
+                }}
+              >
+                {/* left side dropdown button*/}
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+              
+            {/*LOGO*/ }
+            <Link to="/">
+              <h2 className="text-tprimary text-2xl font-bold ">GAMEIT</h2>
+            </Link>
+
+            {/* right side dropdown button*/}
+            <div className="hidden">
+              <button
+                className=" text-primary focus:border-gray-400 p-2 rounded-md outline-none  focus:border"
+                onClick={() => {
+                  setSidebar(!sidebar);
+                  setNavbar(false);
+                }}
+              >
+                {sidebar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-4 h-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          {/* left side wala */}
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-1 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0">
+              <li className="text-tprimary hover:text-tmuted">
+                <Link to="/login">Login</Link>
+              </li>
+              <li className="text-tprimary hover:text-tmuted">
+                <Link to="/help">Help</Link>
+              </li>
+              </ul>
+              </div>
+              {/* {auth.isLoggedIn ? (
+                <>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to={"/profile/" + auth.userEmail} onClick={() => { setNavbar(false) }}>Profile</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/post/new" onClick={() => { setNavbar(false) }}>New Post</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/notes" onClick={() => { setNavbar(false) }}>Notes</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/todo" onClick={() => { setNavbar(false) }}>To-Do List</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/help" onClick={() => { setNavbar(false) }}>Help</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/" onClick={auth.logout}>
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li className="text-tprimary hover:text-tmuted">
+                    <Link to="/help">Help</Link>
+                  </li>
+                </>
+              )} 
+            </ul>
+          </div>
+
+          {/* right side wala menu */}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
