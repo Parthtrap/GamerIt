36bf4c6fce57c0bec45079b0f1588f7c0b3cb3b6
Parthtrap <mttparth306@gmail.com>
2023-03-24 22:27:48 +0530

    API Routes defined

--- server/routers/post.js
+++ server/routers/post.js
@@ -2,29 +2,38 @@ import express from "express";
 const router = express.Router();
 
 // Create a Post
+router.post("/");
 
 // Delete a Post
+router.delete("/");
 
 // Comment on a Post
+router.post("/comment");
 
 // Delete Comment
+router.delete("/comment");
 
 // Like a Comment
+router.post("/likecomment");
 
 // Unlike a Comment
+router.delete("/likecomment");
 
 // Like a Post
+router.post("/like");
 
 // Unlike a Post
+router.post("/unlike");
 
 // Report a Post
+router.post("/report");
 
 // Unreport a Post
+router.delete("/report");
 
 // Search a Post by Title
-
-// Search a Post by Tag
-
 // Search a Post by User
+// Search a Post by Tag
+router.get("/");
 
 export default router;
