2937dd69cd0a901d49f87ed51b980cbf84299a88
Parthtrap <mttparth306@gmail.com>
2023-03-24 22:20:22 +0530

    controllers planing done

---
+++ server/controllers/community.js
@@ -0,0 +1,26 @@
+// Get All Community
+
+
+// Create a Community
+
+// Delete a Community
+
+// Add tag
+
+// Delete tag
+
+// Edit Community Profile Pic
+
+// Edit Community Banner Pic
+
+// Make User Moderator
+
+// Remove User from Moderator
+
+// Pin a Post
+
+// Unpin a Post
+
+// Get all Reported Posts
+
+// Get community by name
---
+++ server/controllers/post.js
@@ -0,0 +1,25 @@
+// Create a Post
+
+// Delete a Post
+
+// Comment on a Post
+
+// Delete Comment
+
+// Like a Comment
+
+// Unlike a Comment
+
+// Like a Post
+
+// Unlike a Post
+
+// Report a Post
+
+// Unreport a Post
+
+// Search a Post by Title
+
+// Search a Post by Tag
+
+// Search a Post by User
