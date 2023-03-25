d820fd3ca8bb824bf2989d4d6ae99969b395212c
Parthtrap <mttparth306@gmail.com>
2023-03-26 03:51:29 +0530

    Create Post Tags Handled

--- client/src/Pages/Components/CreatePostTag.js
+++ client/src/Pages/Components/CreatePostTag.js
@@ -1,36 +1,40 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const CreatePostTag = (props) => {
-    
-    const [selected, setSelected] = useState(false)
+	const [selected, setSelected] = useState(false);
 
-    const mystyle1 = {
-        "background-color": props.tag.color,
-        "border": "2px solid "+ props.tag.color
-    }
-    const mystyle2 = {
-        "border": "2px solid "+ props.tag.color
-    }
+	const mystyle1 = {
+		backgroundColor: props.tag.color,
+		border: "2px solid " + props.tag.color,
+	};
+	const mystyle2 = {
+		border: "2px solid " + props.tag.color,
+	};
 
-    const onClack = () => {
-        
-        if(selected){
-            setSelectedStyle(mystyle2);
-        }else{
-            setSelectedStyle(mystyle1);
-        }
-        setSelected(!selected);
-    }
+	const onClack = () => {
+		if (selected) {
+			setSelectedStyle(mystyle2);
+			props.onRemove(props.tag);
+		} else {
+			setSelectedStyle(mystyle1);
+			props.onSelect(props.tag);
+		}
+		setSelected(!selected);
+	};
 
-    const [selectedStyle, setSelectedStyle] = useState(mystyle2);
-    return (
-        <div className=''>
-            <button onClick={onClack} style={selectedStyle} type="button" className={` px-2 py-1 inline-flex items-center text-sm font-medium text-center text-tprimary rounded-full hover:bg-baccent `}>
-                {props.tag.name}
-            </button>
-        </div>
+	const [selectedStyle, setSelectedStyle] = useState(mystyle2);
+	return (
+		<div className="">
+			<button
+				onClick={onClack}
+				style={selectedStyle}
+				type="button"
+				className={` px-2 py-1 inline-flex items-center text-sm font-medium text-center text-tprimary rounded-full hover:bg-baccent `}
+			>
+				{props.tag.name}
+			</button>
+		</div>
+	);
+};
 
-    )
-}
-
-export default CreatePostTag
+export default CreatePostTag;
