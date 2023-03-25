f8dd715395a4c908ce1382eaf09cbe6f5070f951
Gauarv-Jain <jaing438@gmail.com>
2023-03-26 04:38:17 +0530

    togle button theam continuty

--- client/src/Pages/Communitypage.js
+++ client/src/Pages/Communitypage.js
@@ -272,7 +272,8 @@ function Communitypage() {
               type="checkbox"
               class="sr-only peer"
             />
-            <div class="w-11 h-6 bg-background  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-base"></div>
+            <div class="w-11 h-6 bg-tmuted  rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-base"></div>
+            
             <span class="text-tmuted ml-1 text-sm font-medium ">
               Toggle View
             </span>
